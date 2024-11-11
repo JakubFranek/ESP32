602bfeae6ba9d698e602e27ea0c11286d0b2993d
Jakub Franek <j.franek95@gmail.com>
2024-11-11 22:48:30 +0100

    Improve Blink_LED project
    Properly create a blinking LED task and have the scheduler call it on
    its own in proper RTOS fashion.

--- ESP-ink/Blink_LED/main/main.c
+++ ESP-ink/Blink_LED/main/main.c
@@ -4,14 +4,28 @@
 
 #define LED_PIN 27
 
+void task_toggle_led(void *pvParameters);
+
 void app_main(void)
+{   
+    printf("Setup done");
+
+    xTaskCreatePinnedToCore(task_toggle_led, "Toggle LED", 2048, NULL, 1, NULL, PRO_CPU_NUM);
+
+    // no while-loop needed, as FreeRTOS task scheduler has been already started within esp-idf/components/app_startup.c
+}
+
+void task_toggle_led(void *pvParameters)
 {
     gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_pull_mode(LED_PIN, GPIO_FLOATING);
+
     while (1) {
         gpio_set_level(LED_PIN, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
         gpio_set_level(LED_PIN, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
