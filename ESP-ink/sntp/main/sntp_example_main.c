8cc1c8d87fc92b2b110f82ac786e5df93e3bd85e
Jakub Franek <j.franek95@gmail.com>
2024-11-30 12:43:00 +0100

    Improve sntp project
    Changed timezone to Prague.

--- ESP-ink/sntp/main/sntp_example_main.c
+++ ESP-ink/sntp/main/sntp_example_main.c
@@ -37,9 +37,9 @@ static void obtain_time(void);
 #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_CUSTOM
 void sntp_sync_time(struct timeval *tv)
 {
-   settimeofday(tv, NULL);
-   ESP_LOGI(TAG, "Time is synchronized from custom code");
-   sntp_set_sync_status(SNTP_SYNC_STATUS_COMPLETED);
+    settimeofday(tv, NULL);
+    ESP_LOGI(TAG, "Time is synchronized from custom code");
+    sntp_set_sync_status(SNTP_SYNC_STATUS_COMPLETED);
 }
 #endif
 
@@ -58,14 +58,16 @@ void app_main(void)
     time(&now);
     localtime_r(&now, &timeinfo);
     // Is time set? If not, tm_year will be (1970 - 1900).
-    if (timeinfo.tm_year < (2016 - 1900)) {
+    if (timeinfo.tm_year < (2016 - 1900))
+    {
         ESP_LOGI(TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
         obtain_time();
         // update 'now' variable with current time
         time(&now);
     }
 #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
-    else {
+    else
+    {
         // add 500 ms error to the current system time.
         // Only to demonstrate a work of adjusting method!
         {
@@ -74,7 +76,7 @@ void app_main(void)
             gettimeofday(&tv_now, NULL);
             int64_t cpu_time = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
             int64_t error_time = cpu_time + 500 * 1000L;
-            struct timeval tv_error = { .tv_sec = error_time / 1000000L, .tv_usec = error_time % 1000000L };
+            struct timeval tv_error = {.tv_sec = error_time / 1000000L, .tv_usec = error_time % 1000000L};
             settimeofday(&tv_error, NULL);
         }
 
@@ -87,28 +89,23 @@ void app_main(void)
 
     char strftime_buf[64];
 
-    // Set timezone to Eastern Standard Time and print local time
-    setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
+    // Set timezone to Prague and print local time
+    setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1);
     tzset();
     localtime_r(&now, &timeinfo);
     strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
+    ESP_LOGI(TAG, "The current date/time in Prague is: %s", strftime_buf);
 
-    // Set timezone to China Standard Time
-    setenv("TZ", "CST-8", 1);
-    tzset();
-    localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    ESP_LOGI(TAG, "The current date/time in Shanghai is: %s", strftime_buf);
-
-    if (sntp_get_sync_mode() == SNTP_SYNC_MODE_SMOOTH) {
+    if (sntp_get_sync_mode() == SNTP_SYNC_MODE_SMOOTH)
+    {
         struct timeval outdelta;
-        while (sntp_get_sync_status() == SNTP_SYNC_STATUS_IN_PROGRESS) {
+        while (sntp_get_sync_status() == SNTP_SYNC_STATUS_IN_PROGRESS)
+        {
             adjtime(NULL, &outdelta);
             ESP_LOGI(TAG, "Waiting for adjusting time ... outdelta = %jd sec: %li ms: %li us",
-                        (intmax_t)outdelta.tv_sec,
-                        outdelta.tv_usec/1000,
-                        outdelta.tv_usec%1000);
+                     (intmax_t)outdelta.tv_sec,
+                     outdelta.tv_usec / 1000,
+                     outdelta.tv_usec % 1000);
             vTaskDelay(2000 / portTICK_PERIOD_MS);
         }
     }
@@ -122,10 +119,14 @@ static void print_servers(void)
 {
     ESP_LOGI(TAG, "List of configured NTP servers:");
 
-    for (uint8_t i = 0; i < SNTP_MAX_SERVERS; ++i){
-        if (esp_sntp_getservername(i)){
+    for (uint8_t i = 0; i < SNTP_MAX_SERVERS; ++i)
+    {
+        if (esp_sntp_getservername(i))
+        {
             ESP_LOGI(TAG, "server %d: %s", i, esp_sntp_getservername(i));
-        } else {
+        }
+        else
+        {
             // we have either IPv4 or IPv6 address, let's print it
             char buff[INET6_ADDRSTRLEN];
             ip_addr_t const *ip = esp_sntp_getserver(i);
@@ -137,9 +138,9 @@ static void print_servers(void)
 
 static void obtain_time(void)
 {
-    ESP_ERROR_CHECK( nvs_flash_init() );
+    ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK( esp_event_loop_create_default() );
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
 #if LWIP_DHCP_GET_NTP_SRV
     /**
@@ -153,10 +154,10 @@ static void obtain_time(void)
      */
     ESP_LOGI(TAG, "Initializing SNTP");
     esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG(CONFIG_SNTP_TIME_SERVER);
-    config.start = false;                       // start SNTP service explicitly (after connecting)
-    config.server_from_dhcp = true;             // accept NTP offers from DHCP server, if any (need to enable *before* connecting)
-    config.renew_servers_after_new_IP = true;   // let esp-netif update configured SNTP server(s) after receiving DHCP lease
-    config.index_of_first_server = 1;           // updates from server num 1, leaving server 0 (from DHCP) intact
+    config.start = false;                     // start SNTP service explicitly (after connecting)
+    config.server_from_dhcp = true;           // accept NTP offers from DHCP server, if any (need to enable *before* connecting)
+    config.renew_servers_after_new_IP = true; // let esp-netif update configured SNTP server(s) after receiving DHCP lease
+    config.index_of_first_server = 1;         // updates from server num 1, leaving server 0 (from DHCP) intact
     // configure the event on which we renew servers
 #ifdef CONFIG_EXAMPLE_CONNECT_WIFI
     config.ip_event_to_renew = IP_EVENT_STA_GOT_IP;
@@ -182,10 +183,11 @@ static void obtain_time(void)
      * (statically assigned IPv6 address is also possible)
      */
     ip_addr_t ip6;
-    if (ipaddr_aton("2a01:3f7::1", &ip6)) {    // ipv6 ntp source "ntp.netnod.se"
+    if (ipaddr_aton("2a01:3f7::1", &ip6))
+    { // ipv6 ntp source "ntp.netnod.se"
         esp_sntp_setserver(2, &ip6);
     }
-#endif  /* LWIP_IPV6 */
+#endif /* LWIP_IPV6 */
 
 #else
     ESP_LOGI(TAG, "Initializing and starting SNTP");
@@ -193,14 +195,14 @@ static void obtain_time(void)
     /* This demonstrates configuring more than one server
      */
     esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG_MULTIPLE(2,
-                               ESP_SNTP_SERVER_LIST(CONFIG_SNTP_TIME_SERVER, "pool.ntp.org" ) );
+                                                                      ESP_SNTP_SERVER_LIST(CONFIG_SNTP_TIME_SERVER, "pool.ntp.org"));
 #else
     /*
      * This is the basic default config with one server and starting the service
      */
     esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG(CONFIG_SNTP_TIME_SERVER);
 #endif
-    config.sync_cb = time_sync_notification_cb;     // Note: This is only needed if we want
+    config.sync_cb = time_sync_notification_cb; // Note: This is only needed if we want
 #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
     config.smooth_sync = true;
 #endif
@@ -212,15 +214,16 @@ static void obtain_time(void)
 
     // wait for time to be set
     time_t now = 0;
-    struct tm timeinfo = { 0 };
+    struct tm timeinfo = {0};
     int retry = 0;
     const int retry_count = 15;
-    while (esp_netif_sntp_sync_wait(2000 / portTICK_PERIOD_MS) == ESP_ERR_TIMEOUT && ++retry < retry_count) {
+    while (esp_netif_sntp_sync_wait(2000 / portTICK_PERIOD_MS) == ESP_ERR_TIMEOUT && ++retry < retry_count)
+    {
         ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
     }
     time(&now);
     localtime_r(&now, &timeinfo);
 
-    ESP_ERROR_CHECK( example_disconnect() );
+    ESP_ERROR_CHECK(example_disconnect());
     esp_netif_sntp_deinit();
 }
