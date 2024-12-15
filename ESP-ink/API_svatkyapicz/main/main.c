38abedc245fbe09050dffb5d25b23f6613b6873d
Jakub Franek <j.franek95@gmail.com>
2024-12-15 17:57:36 +0100

    Improve API_svatkyapicz project
    Add comments.
    Improve formatting.
    Set debug log level.

--- ESP-ink/API_svatkyapicz/main/main.c
+++ ESP-ink/API_svatkyapicz/main/main.c
@@ -36,32 +36,34 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     case HTTP_EVENT_ERROR:
         ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
         break;
+
     case HTTP_EVENT_ON_CONNECTED:
         ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
         break;
+
     case HTTP_EVENT_HEADER_SENT:
         ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
         break;
+
     case HTTP_EVENT_ON_HEADER:
         ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
         break;
+
     case HTTP_EVENT_ON_DATA:
         ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+
         // Clean the buffer in case of a new request
         if (output_len == 0 && evt->user_data)
         {
             // we are just starting to copy the output data into the use
             memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
         }
-        /*
-         *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
-         *  However, event handler can also be used in case chunked encoding is used.
-         */
+
         if (!esp_http_client_is_chunked_response(evt->client))
         {
-            // If user_data buffer is configured, copy the response into the buffer
             int copy_len = 0;
-            if (evt->user_data)
+
+            if (evt->user_data) // If user_data buffer is configured, copy the response into the buffer
             {
                 // The last byte in evt->user_data is kept for the NULL character in case of out-of-bound access.
                 copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
@@ -70,9 +72,10 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
                     memcpy(evt->user_data + output_len, evt->data, copy_len);
                 }
             }
-            else
+            else // If user_data buffer is not configured, accumulate the response in output_buffer
             {
                 int content_len = esp_http_client_get_content_length(evt->client);
+
                 if (output_buffer == NULL)
                 {
                     // We initialize output_buffer with 0 because it is used by strlen() and similar functions therefore should be null terminated.
@@ -84,6 +87,7 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
                         return ESP_FAIL;
                     }
                 }
+
                 copy_len = MIN(evt->data_len, (content_len - output_len));
                 if (copy_len)
                 {
@@ -92,23 +96,25 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
             }
             output_len += copy_len;
         }
-        else
+        else // Chunked encoding
         {
-            ESP_LOGI(TAG, "Received chunk: %.*s", evt->data_len, (char *)evt->data);
+            ESP_LOGI(TAG, "Received data chunk = %.*s", evt->data_len, (char *)evt->data);
         }
 
         break;
+
     case HTTP_EVENT_ON_FINISH:
         ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
         if (output_buffer != NULL)
         {
             // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
-            // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+            ESP_LOGI(TAG, "Accumulated Response = %.*s", output_len, output_buffer);
             free(output_buffer);
             output_buffer = NULL;
         }
         output_len = 0;
         break;
+
     case HTTP_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
         int mbedtls_err = 0;
@@ -125,6 +131,7 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
         }
         output_len = 0;
         break;
+
     case HTTP_EVENT_REDIRECT:
         ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
         esp_http_client_set_header(evt->client, "From", "user@example.com");
@@ -163,6 +170,8 @@ static void https_with_url(void)
 
 void app_main(void)
 {
+    esp_log_level_set("*", ESP_LOG_DEBUG);
+
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
